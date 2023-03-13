0ede7134b735d828ac3156cf1fd0151a8c2ffd55
massone99 <lorenzomassone@gmail.com>
2023-03-13 11:53:26 +0100

    Exercises 1-4

---
+++ src/u02/Experimentation.scala
@@ -0,0 +1,31 @@
+package u02
+
+val positiveInline: Int => String = (x: Int) =>
+  x match
+    case x if x > 0 => "positive"
+    case x if x < 0 => "negative"
+
+def positive(x: Int): String = x match
+  case x if x > 0 => "positive"
+  case x if x < 0 => "negative"
+
+val negInline: (String => Boolean) => String => Boolean =
+  (f: String => Boolean) => (s: String) => !f(s)
+
+def neg(pred: String => Boolean): String => Boolean =
+  (s: String) => !pred(s)
+
+def genericNeg[A](pred: A => Boolean) =
+  (s: A) => !pred(s)
+
+val thernaryRelationCurried: Int => Int => Int => Boolean =
+  (x: Int) => (y: Int) => (z: Int) => x <= y && y == z
+
+val thernaryRelationUncurried: (Int, Int, Int) => Boolean =
+  (x: Int, y: Int, z: Int) => x <= y && y == z
+
+def thernaryRelationMethodCurried(x: Int)(y: Int)(z: Int): Boolean =
+  x <= y && y == z
+
+def thernaryRelationMethodUncurried(x: Int, y: Int, z: Int): Boolean =
+  x <= y && y == z
---
+++ test/u02/ExperimentationTest.scala
@@ -0,0 +1,44 @@
+package u02
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ExperimentationTest {
+
+  val testString = "ciao"
+  val notEmptyPred: (String => Boolean) = s => s.isEmpty
+
+  @Test def testPositiveInline(): Unit =
+    val x: Int = 1
+    val y: Int = -1
+    assertEquals("positive", positiveInline(x))
+    assertEquals("negative", positiveInline(y))
+  @Test def testPositive(): Unit =
+    val x: Int = 1
+    val y: Int = -1
+    assertEquals("positive", positive(x))
+    assertEquals("negative", positive(y))
+
+  @Test def testNegInline(): Unit =
+    assertTrue(negInline(notEmptyPred)(testString))
+
+  @Test def testNegMethod(): Unit =
+    assertTrue(neg(notEmptyPred)(testString))
+
+  @Test def testGenericNeg(): Unit =
+    def genericIsInt[A](x : A): Boolean = x.isInstanceOf[Int]
+    val x = 20
+    assertFalse(genericNeg(genericIsInt)(x))
+
+  @Test def testThernaryRelationCurried(): Unit =
+    val x = 1
+    val y = 2
+    val z = 2
+    assertTrue(thernaryRelationCurried(x)(y)(z))
+    assertTrue(thernaryRelationUncurried(x, y, z))
+    assertTrue(thernaryRelationMethodCurried(x)(y)(z))
+    assertTrue(thernaryRelationMethodUncurried(x, y, z))
+
+
+
+}
